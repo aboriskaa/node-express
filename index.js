60e20ff971435125dc14e51d48e1ee2fa7a0dbdb
aboriska <aboriska@yahoo.com>
2022-07-08 17:36:58 +0300

    prepair cart

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars');
 const homeRoutes = require('./routes/home');
 const coursesRoutes = require('./routes/courses');
 const addRoutes = require('./routes/add');
+const cardRoutes = require('./routes/card');
 
 const PORT = process.env.PORT || 3000;
 
@@ -25,6 +26,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/', homeRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/add', addRoutes);
+app.use('/card', cardRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server strated on port ${PORT}`);
