858300de7f11f26f141c8570c9af8cc16eaf9e87
aboriska <aboriska@yahoo.com>
2022-07-15 20:43:57 +0300

    add products

--- routes/card.js
+++ routes/card.js
@@ -1,18 +1,17 @@
 const { Router } = require('express');
-const Card = require('../models/card')
-const Course = require('../models/course')
+const Course = require('../models/course');
 const router = Router();
 
 router.post('/add', async (req, res) => {
-    const course = await Course.getById(req.body.id);
-    await Card.add(course);
+    const course = await Course.findById(req.body.id);
+    await req.user.addToCart(course);
     res.redirect('/card');
 
 })
 
 router.delete('/remove/:id', async (req, res) => {
     const card = await Card.remove(req.params.id);
-    res.status(200).json(card)
+    res.status(200).json(card);
 })
 
 router.get('/', async (req, res) => {
@@ -26,4 +25,4 @@ router.get('/', async (req, res) => {
     })
 })
 
-module.exports = router
+module.exports = router;
