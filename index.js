020d89e4df9092a177e0cc6c877f66d2b1cf9bcc
aboriska <aboriska@yahoo.com>
2022-07-15 19:46:00 +0300

    add user

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const coursesRoutes = require('./routes/courses');
 const addRoutes = require('./routes/add');
 const cardRoutes = require('./routes/card');
 
+const User = require('./models/user')
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -27,6 +29,17 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
+app.use(async (req, res, next) => {
+    try {
+        const user = await User.findById('62d18eb8db2c8c39e1cb2230');
+        req.user = user;
+        next();
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: true }))
@@ -40,6 +53,17 @@ async function start() {
     try {
         const url = "mongodb+srv://boris:uwz8lfXbx6NIhXtx@cluster0.byvu1.mongodb.net/shop"
         await mongoose.connect(url, { useNewUrlParser: true });
+
+        const candidate = await User.findOne()
+        if (!candidate) {
+            const user = new User({
+                email: '9600929@gmail.com',
+                name: 'Boris',
+                cart: { items: [] }
+            })
+            await user.save()
+        }
+
         app.listen(PORT, () => {
             console.log(`My server strated on port ${PORT}`);
         })
