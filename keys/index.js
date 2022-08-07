4cbb72db6f9b2b9fb3b6cbbe03f527acc6f8b66c
Boris <42873497+aboriskaa@users.noreply.github.com>
2022-08-07 18:40:55 +0300

    Delete index.js

--- keys/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    MONGODB_URI: 'mongodb+srv://boris:uwz8lfXbx6NIhXtx@cluster0.byvu1.mongodb.net/shop',
-    SESSION_SECRET: 'some secret value',
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: 'aboriska87@gmail.com',
-    BASE_URL: 'http://localhost:3000'
-}
